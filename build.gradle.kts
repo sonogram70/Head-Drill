cd1000f9ac9be6bf26c687d2a8ebea874beabc13
sonogram70 <sonogram70@gmail.com>
2021-02-28 03:42:54 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.4.31"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
+}
+
+group = "com.github.sonogram"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://jitpack.io/")
+    maven("https://papermc.io/repo/repository/maven-public")
+}
+
+dependencies {
+    compileOnly(kotlin("stdlib"))
+    compileOnly("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
+    implementation("com.github.monun:tap:3.3.3")
+}
+tasks {
+    create<Copy>("copyToServer") {
+        from(shadowJar)
+        var dest = File(rootDir, ".server/plugins")
+        if(File(rootDir, shadowJar.get().archiveFileName.get()).exists()) dest = File(dest, "update")
+        into(dest)
+    }
+}
---
+++ src/main/kotlin/com/github/sonogram/sampleplugin/SamplePlugin.kt
@@ -0,0 +1,5 @@
+package com.github.sonogram.sampleplugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SamplePlugin: JavaPlugin()
